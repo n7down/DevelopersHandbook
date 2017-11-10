77e1a30fe5c83f171159866541526e55d62cc21c
Joshua Adams <n7down@gmail.com>
2017-11-10 10:58:08 -0700

    added more test cases

--- CodeKata/Algorithms/Arrays/MaximumSubarray/MaximumSubarray.Tests/FindMaximumSubarrayTests.cs
+++ CodeKata/Algorithms/Arrays/MaximumSubarray/MaximumSubarray.Tests/FindMaximumSubarrayTests.cs
@@ -28,5 +28,23 @@ namespace MaximumSubarray.Tests
         {
             Assert.Throws<ArgumentException>(() => Subarray.FindLargestSumOfContiguousSubarray(i));
         }
+
+        [Theory]
+        [InlineData(new int[] { -2, -1, -3 }, -1)]
+        [InlineData(new int[] { -1, -2, -3, -6, -13 }, -1)]
+        public void Given_ArrayWithNegitiveValues_Expect_NumberInArrayWithSmallestMagnitude(int[] i, int e)
+        {
+            int a = Subarray.FindLargestSumOfContiguousSubarray(i);
+            Assert.True(a == e, "Actual: " + a);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 6 }, 9)]
+        [InlineData(new int[] { 2, 6 }, 8)]
+        public void Given_ArrayWithPositiveValues_Expect_NumberInArrayWithSmallestMagnitude(int[] i, int e)
+        {
+            int a = Subarray.FindLargestSumOfContiguousSubarray(i);
+            Assert.True(a == e, "Actual: " + a);    
+        }
     }
 }
