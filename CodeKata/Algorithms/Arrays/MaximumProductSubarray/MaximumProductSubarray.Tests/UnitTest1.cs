102cb30b4687ea73863e64ad99d838cf38d677cb
Joshua Adams <n7down@gmail.com>
2017-11-11 14:57:14 -0700

    added new kata

---
+++ CodeKata/Algorithms/Arrays/MaximumProductSubarray/MaximumProductSubarray.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MaximumProductSubarray.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CodeKata/Algorithms/Arrays/MaximumProductSubarray/MaximumProductSubarray/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MaximumProductSubarray
+{
+    public class Class1
+    {
+    }
+}
