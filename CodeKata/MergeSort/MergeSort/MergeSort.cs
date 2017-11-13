42a634d3dd67def5f54767cc850fc8733f9cca66
Joshua Adams <n7down@gmail.com>
2017-11-13 10:21:21 -0700

    got test to pass

--- CodeKata/MergeSort/MergeSort/MergeSort.cs
+++ CodeKata/MergeSort/MergeSort/MergeSort.cs
@@ -6,6 +6,14 @@ namespace MergeSort
     {
         public static int[] Sort(int[] i)
         {
+            if(i == null || i.Length == 0)
+            {
+                throw new ArgumentException("array is null or empty");
+            }
+            if(i.Length == 1)
+            {
+                return i;
+            }
             return new int[] {};
         }
     }
