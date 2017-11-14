840434984fdb6d02daa4ec0c57fdd5f3d048e063
Joshua Adams <n7down@gmail.com>
2017-11-13 18:01:40 -0700

    updated

--- CodeKata/MergeSort/MergeSort/MergeSort.cs
+++ CodeKata/MergeSort/MergeSort/MergeSort.cs
@@ -4,6 +4,24 @@ namespace MergeSort
 {
     public class MergeSort
     {
+        private static int[] Sort(int[] i, int l, int r)
+        {
+            if(i[r] > i[l])
+            {
+                int m = (l + r) / 2;
+                int[] la = Sort(i, l, m);
+                int[] ra = Sort(i, m + 1, r);
+                int[] result = Merge(i, l, m, r);
+                return result;
+            }
+            return new int[] {};
+        }
+
+        private static int[] Merge(int[] i, int l, int m, int r)
+        {
+            return new int[] {};
+        }
+
         public static int[] Sort(int[] i)
         {
             if(i == null || i.Length == 0)
@@ -14,7 +32,7 @@ namespace MergeSort
             {
                 return i;
             }
-            return new int[] {};
+            return Sort(i, 0, i.Length - 1);
         }
     }
 }
