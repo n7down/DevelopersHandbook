59afcab2213abd82dfab05181ca543d7b8efaeaf
Joshua Adams <n7down@gmail.com>
2017-11-16 23:09:45 -0700

    updated

--- CodeKata/Algorithms/Arrays/MaximumProductSubarray/MaximumProductSubarray.Tests/UnitTest1.cs
+++ CodeKata/Algorithms/Arrays/MaximumProductSubarray/MaximumProductSubarray.Tests/UnitTest1.cs
@@ -5,10 +5,21 @@ namespace MaximumProductSubarray.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(new int[] {})]
+        public void Given_EmptyOrNullArray_Expect_ThrowArgumentException(int[] i)
         {
+            Assert.Throws<ArgumentException>(() => Subarray.MaximumProduct(i));
+        }
 
+        [Theory]
+        [InlineData(new int[] { 1 })]
+        [InlineData(new int[] { 2, 4 }, 8)]
+        public void Given_ValidArray_Expect_ReturnMaximumProduct(int[] i, int e)
+        {
+            int a = Subarray.MaximumProduct(i);
+            Assert.True(e == a);
         }
     }
 }
