bfa0333d47e6bdf5c88eee2fb4a818e3dfff2ccb
Joshua Adams <n7down@gmail.com>
2017-11-19 00:16:21 -0700

    updated kata

--- CodeKata/BubbleSort/BubbleSort/BubbleSort.cs
+++ CodeKata/BubbleSort/BubbleSort/BubbleSort.cs
@@ -4,6 +4,13 @@ namespace BubbleSort
 {
     public class BubbleSort
     {
+        private static void Swap(int[] d, int a, int b)
+        {
+            int tmp = d[a];
+            d[a] = d[b];
+            d[b] = tmp;
+        }
+
         public static int[] Sort(int[] d)
         {
             return new int[] {};
