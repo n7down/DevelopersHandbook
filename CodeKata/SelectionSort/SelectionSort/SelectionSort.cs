ae358772262f5342be3d41235bbe30ff44404913
Joshua Adams <n7down@gmail.com>
2017-11-26 22:17:18 -0700

    started working on kata

--- CodeKata/SelectionSort/SelectionSort/SelectionSort.cs
+++ CodeKata/SelectionSort/SelectionSort/SelectionSort.cs
@@ -6,6 +6,22 @@ namespace SelectionSort
     {
         public static int[] Sort(int[] d)
         {
+            if(d == null || d.Length == 0)
+            {
+                throw new ArgumentException("null or empty array");
+            }
+            if(d.Length == 1)
+            {
+                return d;
+            }
+            for(int i = 0; i < d.Length - 1; i++)
+            {
+                int min = d[i];
+                for(int j = i; j < d.Length; j++)
+                {
+                    // TODO
+                }
+            }
             return new int[] {};
         }
     }
