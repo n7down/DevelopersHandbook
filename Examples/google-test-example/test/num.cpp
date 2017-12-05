dca8e997dc9e19729bbbab01450d9355125e9025
Joshua Adams <n7down@gmail.com>
2017-12-05 16:37:23 -0700

    trying to work with src folder

--- Examples/google-test-example/test/num.cpp
+++
@@ -1,9 +0,0 @@
-#include "num.h"
-
-Num::Num() : num(0) {}
-Num::Num(int n) : num(n) {}
-
-int Num::getNum()
-{ 
-	return num; 
-}
--- Examples/google-test-example/test/num.h
+++
@@ -1,9 +0,0 @@
-class Num
-{
-	private:
-		int num;
-	public:
-		Num();
-		Num(int n);
-		int getNum();
-};
