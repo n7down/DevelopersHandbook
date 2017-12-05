c1ba82bc1e32f2caaf8b1c975e3bc7f9c0f86ca0
Joshua Adams <n7down@gmail.com>
2017-12-05 15:04:53 -0700

    trying to get files to build from src folder

--- Examples/google-test-example/test/num_unittest.cc
+++ Examples/google-test-example/test/num_unittest.cc
@@ -6,13 +6,13 @@ namespace
 {
 	TEST(Num, DefaultConstructor)
 	{
-		const Num n;
+		Num n;
 		EXPECT_EQ(0, n.getNum()); 
 	}
 
 	TEST(Num, Set)
 	{
-		const Num n(1);
-		EXPECT_EQ(1, n.getNum();
+		Num n(1);
+		EXPECT_EQ(1, n.getNum());
 	}
 }
