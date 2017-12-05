de1ac8d43a2234155f32e1f13b8233501291e136
Joshua Adams <n7down@gmail.com>
2017-12-05 14:16:17 -0700

    updated unit test

--- Examples/google-test-example/test/num_unittest.cc
+++ Examples/google-test-example/test/num_unittest.cc
@@ -1,2 +1,18 @@
 #include <gtest/gtest.h>
 
+#include "num.h"
+
+namespace 
+{
+	TEST(Num, DefaultConstructor)
+	{
+		const Num n;
+		EXPECT_EQ(0, n.getNum()); 
+	}
+
+	TEST(Num, Set)
+	{
+		const Num n(1);
+		EXPECT_EQ(1, n.getNum();
+	}
+}
