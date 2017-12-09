dd474e7cc6f49224d1b0a1b04fb182f337682a4c
Joshua Adams <n7down@gmail.com>
2017-12-09 14:38:34 -0700

    fixed tests and updated cmake

---
+++ CodeKata/CPP/InsertionSort/test/insertionsort_unittest.cc
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include "insertionsort.h"
+
+namespace
+{
+	TEST(InsertionSort, DefaultConstructor)
+	{
+		ASSERT_TRUE(false);
+	}
+
+	TEST(InsertionSort, Set)
+	{
+		ASSERT_TRUE(false);
+	}
+}
