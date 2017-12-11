9e526b42a784c5ca4cc8c8bfa61384490ccd41f4
Joshua Adams <n7down@gmail.com>
2017-12-11 10:38:45 -0700

    updated tests

--- CodeKata/CPP/InsertionSort/test/insertionsort_unittest.cc
+++ CodeKata/CPP/InsertionSort/test/insertionsort_unittest.cc
@@ -1,13 +1,20 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
+#include <string>
 
 #include "insertionsort.h"
 
 namespace
 {
-	bool isEqual(int a[], int b[], int size)
+	bool isEqual(int a[], int b[])
 	{
-		for(int i = 0; i < size; i++)
+		int aSize = *(&a + 1) - a; 
+		int bSize = *(&b + 1) - b;
+		if(aSize != bSize)
+		{
+			return false;
+		}
+		for(int i = 0; i < aSize; i++)
 		{
 			if(a[i] != b[i])
 			{
@@ -17,7 +24,12 @@ namespace
 		return true;
 	}
 
-	TEST(InsertionSortTest, EmptyArray)
+	std::string printArray(int d[])
+	{
+		int size = *(&d + 1) - d;	
+	}
+
+	TEST(InsertionSort, EmptyArray)
 	{
 		try {
 			int d[] = {};
@@ -32,19 +44,19 @@ namespace
 		}
 	}
 
-	TEST(InsertionSortTest, ArrayWithOneElement)
+	TEST(InsertionSort, ArrayWithOneElement)
 	{
 		int d[] = { 1 };
 		int e[] = { 1 };
 		InsertionSort::sort(d, 1);
-		ASSERT_TRUE(isEqual(d, e, 1));
+		ASSERT_TRUE(isEqual(d, e)) << "error";
 	}
 
-	TEST(InsertionSortTest, ArrayWithTwoElements)
+	TEST(InsertionSort, ArrayWithTwoElements)
 	{
 		int d[] = { 2, 1 };
 		int e[] = { 1, 2 };
 		InsertionSort::sort(d, 2);
-		ASSERT_TRUE(isEqual(d, e, 2));
+		ASSERT_TRUE(isEqual(d, e)) << "error";
 	}
 }
