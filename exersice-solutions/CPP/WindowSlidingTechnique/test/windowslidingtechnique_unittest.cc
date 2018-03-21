7eb5e1232d9c14c69780a4d3529a954b4f10740c
Joshua Adams <n7down@gmail.com>
2018-03-21 14:07:40 -0600

    added test cases

--- exersice-solutions/CPP/WindowSlidingTechnique/test/windowslidingtechnique_unittest.cc
+++ exersice-solutions/CPP/WindowSlidingTechnique/test/windowslidingtechnique_unittest.cc
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
+#include <string>
 
 #include "windowslidingtechnique.h"
 
@@ -8,6 +9,25 @@ TEST(WindowSlidingTechniqueTest, InvalidInput)
 	int i[] = { 2, 3 };
 	int k = 3;
 	int s = 2;
-	int e = -1;
 	ASSERT_THROW(WindowSlidingTechnique::maxSum(i, s, k), std::invalid_argument);
+}
+
+TEST(WindowSlidingTechniqueTest, MaxSumWhereKIs2)
+{
+	int i[] = { 100, 200, 300, 400 };
+	int k = 2;
+	int s = 4;
+	int e = 700;
+	int a = WindowSlidingTechnique::maxSum(i, s, k);
+	ASSERT_TRUE(a == e) << "actual: " + std::to_string(a);
+}
+
+TEST(WindowSlidingTechnique, MaxSumWhereKIs4)
+{
+	int i[] = { 1, 4, 2, 10, 23, 3, 1, 0, 20 };
+	int k = 4;
+	int s = 9;
+	int e = 39;
+	int a = WindowSlidingTechnique::maxSum(i, s, k);
+	ASSERT_TRUE(a == e) << "actual: " + std::to_string(a);
 }
