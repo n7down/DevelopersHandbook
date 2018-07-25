adb8a904487ca06861ba778f9c64249859a0e803
Joshua Adams <n7down@gmail.com>
2018-07-25 11:35:05 -0600

    started to work on kata

--- exersice-solutions/cracking-the-coding-interview/chapter-3/src/stack.h
+++ exersice-solutions/cracking-the-coding-interview/chapter-3/src/stack.h
@@ -4,36 +4,67 @@
 #include "node.h"
 
 #include <string>
+#include <excption>
+#include <sstream>
 
 template <class T>
 class Stack
 {
 private:
 	Node<T> *head;
+	T d;
+	int c;
 
 public:
-	Stack()
+	Stack(T data)
 	{
 		this->head = 0;
+		this->d = data;
+		this->c = 0;
+	}
+
+	bool isEmpty()
+	{
+		if(c == 0)
+		{
+			return true;
+		}
+		return false;
 	}
 
 	T pop()
-	{}
+	{
+		if(isEmpty)
+		{
+			// throw exception
+		}
+		c--;
+	}
 
 	void push(T data)
-	{}
+	{
+		c++;
+	}
 
 	T peek()
 	{}
 
-	bool isEmpty()
-	{}
-
-	bool isEqual()
+	bool isEqual(Stack<T> s)
 	{}
 
 	std::string print()
-	{}
+	{
+		Node<T> *current = head;
+		std::stringstream ss;
+		ss << "[ ";
+		while(current != NULL)
+		{
+			ss << current->data << " ";
+			current = current->next;
+		}
+		ss << "]";
+		return ss.str();
+	}
 };
 
 #endif
