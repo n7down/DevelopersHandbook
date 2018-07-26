68a360b6895cee85c33ae0b9bbecb08ce7f41497
Joshua Adams <n7down@gmail.com>
2018-07-26 11:33:12 -0600

    working on queue kata

--- exersice-solutions/cracking-the-coding-interview/chapter-3/src/queue.h
+++ exersice-solutions/cracking-the-coding-interview/chapter-3/src/queue.h
@@ -4,6 +4,8 @@
 #include "node.h"
 
 #include <string>
+#include <sstream>
+#include <exception>
 
 template <class T>
 class Queue
@@ -11,31 +13,95 @@ class Queue
 private:
 	Node<T> *head;
 	Node<T> *tail;
+	int c;
 
 public:
 	Queue()
 	{
 		this->head = 0;
 		this->tail = 0;
+		this->c = 0;
+	}
+
+	bool isEmpty() const
+	{
+		if(c == 0)
+		{
+			return true;
+		}
+		return false;
 	}
 
 	void add(T data)
-	{}
+	{
+		c++;
+	}
 
 	T remove()
-	{}
+	{
+		c--;
+	}
 
 	T peek()
-	{}
+	{
+		if(this->head == NULL)
+		{
+			throw std::length_error("queue is empty");
+		}
+		return this->head->data;
+	}
 
-	bool isEmpty()
-	{}
+	bool isEqual(const Queue<T> &q) const
+	{
+		Node<T> *ch = this->head;
+		Node<T> *lh = q.head;
 
-	bool isEqual()
-	{}
+		while(ch && lh)
+		{
+			if(ch->data != lh->data)
+			{
+				return false;
+			}
 
-	std::string print()
-	{}
+			ch = ch->next;
+			lh = lh->next;
+		}
+
+		if(ch && !lh)
+		{
+			return false;
+		}
+
+		if(!ch && lh)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	bool operator ==(const Queue<T> &q) const
+	{
+		return isEquals(q);
+	}
+
+	bool operator !=(const Queue<T> &q) const
+	{
+		return !isEquals(q);
+	}
+
+	std::string print() const
+	{
+		std::stringstream ss;
+		Node<T> *current = this->head;
+		ss << "[ ";
+		while(current)
+		{
+			ss << std::to_string(current->data) << " ";
+			current = current->next;
+		}
+		ss << "]";
+		return ss.str();
+	}
 };
 
 #endif
--- exersice-solutions/cracking-the-coding-interview/chapter-3/test/queue_unittest.cc
+++ exersice-solutions/cracking-the-coding-interview/chapter-3/test/queue_unittest.cc
@@ -15,7 +15,32 @@ protected:
 public:
 };
 
-TEST(QueueTest, failing_test)
+TEST_F(QueueTest, check_add_operator_adds_elements_to_a_queue)
+{
+	EXPECT_TRUE(false);
+}
+
+TEST_F(QueueTest, check_remove_operator_removes_elements_from_a_queue)
+{
+	EXPECT_TRUE(false);
+}
+
+TEST_F(QueueTest, check_peek_operator_shows_first_element_in_queue)
+{
+	EXPECT_TRUE(false);
+}
+
+TEST_F(QueueTest, check_isEmpty_operator_checks_if_queue_is_empty)
+{
+	EXPECT_TRUE(false);
+}
+
+TEST_F(QueueTest, compare_two_queues_are_equal)
+{
+	EXPECT_TRUE(false);
+}
+
+TEST_F(QueueTest, compare_two_queues_are_not_equal)
 {
 	EXPECT_TRUE(false);
 }
