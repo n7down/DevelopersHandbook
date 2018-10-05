5787b14e52894a6547a8490f48f379368b91e1f5
Joshua Adams <n7down@gmail.com>
2018-10-04 23:28:59 -0600

    updated

--- exersice-solutions/cracking-the-coding-interview/CSharp/chapter-1/Chapter1/ PalindromePermutation.cs
+++ exersice-solutions/cracking-the-coding-interview/CSharp/chapter-1/Chapter1/ PalindromePermutation.cs
@@ -1,9 +1,25 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 public static class PalindromePermutation
 {
+    public static IEnumerable<string> GetPermutations(string s)
+    {
+        // permutations - https://stackoverflow.com/questions/16907040/how-to-use-query-syntax-to-create-permutations
+        if (s.Length == 1) 
+        {
+            return new List<string> { s };
+        }
+        var permutations = from c in s
+                        from p in GetPermutations(new String(s.Where(x => x != c).ToArray()))
+                        select c + p;     
+
+        return permutations;
+    }
+
     public static bool CheckPalindromePermutation(string s, string p)
     {
         var isPalindrome = false;
@@ -22,6 +38,9 @@ public static class PalindromePermutation
             second = second.Substring(0, second.Length / 2);
             isPalindrome = first.Equals(second);
         }
+
+        
+
         return isPalindrome && isPermutation;
     }
 }
