f954cc0782e16e8c61f6c35afb264bf9bf3af7b5
Joshua Adams <n7down@gmail.com>
2018-10-08 16:27:30 -0600

    working on kata

--- exersice-solutions/cracking-the-coding-interview/CSharp/chapter-1/Chapter1.Tests/RotateMatrixTests.cs
+++ exersice-solutions/cracking-the-coding-interview/CSharp/chapter-1/Chapter1.Tests/RotateMatrixTests.cs
@@ -1,22 +1,34 @@
+using System.Linq;
 using Xunit;
 
 namespace Chapter1.Tests
 {
     public class RotateMatrixTests
     {
+        private bool Equals(int[,] d, int[,] e)
+        {
+            return d.Rank == e.Rank &&
+                Enumerable.Range(0,d.Rank).All(dimension => d.GetLength(dimension) == e.GetLength(dimension)) &&
+                d.Cast<int>().SequenceEqual(e.Cast<int>());
+        }
+
         [Fact]
         public void Given_A_1x1_Matrix_Rotate90Degrees_Will_Return_A_Matrix_90_Degrees()
         {
             int[,] data = new int[,] { { 1 } };
             int[,] expected = new int[,] { { 1 } };
+            int[,] actual = RotateMatrix.Rotate90Degrees(data);
+            Assert.True(Equals(expected, actual));
         }
 
         [Fact]
         public void Given_A_2x1_Matrix_Rotate90Degrees_Will_Return_A_Matrix_90_Degrees()
         {
             int[,] data = new int[,] { { 1, 2 } };
-            int[,] expected = new int[,] { { 1 }, 
-                                           { 2 } };
+            int[,] expected = new int[,] { { 2 }, 
+                                           { 1 } };
+            int[,] actual = RotateMatrix.Rotate90Degrees(data);
+            Assert.True(Equals(expected, actual));
         }
 
         [Fact]
@@ -24,8 +36,22 @@ namespace Chapter1.Tests
         {
             int[,] data = new int[,] { { 1, 2 }, 
                                        { 3, 4 } };
-            int[,] expected = new int[,] { { 3, 1 }, 
-                                           { 4, 2 } };
+            int[,] expected = new int[,] { { 2, 4 }, 
+                                           { 1, 3 } };
+            int[,] actual = RotateMatrix.Rotate90Degrees(data);
+            Assert.True(Equals(expected, actual));
+        }
+
+        [Fact]
+        public void Given_A_3x2_Matrix_Rotate90Degrees_Will_Return_A_Matrix_90_Degrees()
+        {
+            int[,] data = new int[,] { { 1, 2, 3 }, 
+                                       { 4, 5, 6 } };
+            int[,] expected = new int[,] { { 3, 6 }, 
+                                           { 2, 5 },
+                                           { 1, 4 } };
+            int[,] actual = RotateMatrix.Rotate90Degrees(data);
+            Assert.True(Equals(expected, actual));
         }
     }
 }
--- exersice-solutions/cracking-the-coding-interview/CSharp/chapter-1/Chapter1/RotateMatrix.cs
+++ exersice-solutions/cracking-the-coding-interview/CSharp/chapter-1/Chapter1/RotateMatrix.cs
@@ -1,10 +1,26 @@
+using System.Collections.Generic;
+
 namespace Chapter1
 {
     public static class RotateMatrix
     {
-        public static void Rotate90Degrees()
+        public static int[,] Rotate90Degrees(int[,] data)
         {
-            
+            int rowLength = data.GetLength(0);
+            int columnLength = data.GetLength(1);
+            if(rowLength == 1 && columnLength == 1)
+            {
+                return data;
+            }
+            Dictionary<int, string> t = new Dictionary<int, string>();
+            for(int i = 0; i < rowLength; i++)
+            {
+                for(int j = 0; j < columnLength; j++)
+                {
+                    var d = data[i, j];
+                }
+            }
+            return new int[,] {};
         }
     }
 }
